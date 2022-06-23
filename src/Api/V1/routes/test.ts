ea329574fbdd65dc64c9c7501f819504e67aaa01
AkbarKamran007 <akbar.neeo@gmail.com>
2022-06-23 12:59:01 +0500

    ok

--- src/Api/V1/routes/test.ts
+++ src/Api/V1/routes/test.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response, NextFunction } from "express";
 const router = Router();
-router.post("/test", (req: Request, res: Response) => {
+router.get("/test", (req: Request, res: Response) => {
   res.status(200).json({ status: "success", message: "Welcome Tourish" });
 });
 
