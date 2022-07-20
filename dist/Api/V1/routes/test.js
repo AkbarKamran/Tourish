f4e1e5cc5d8817294de68c1eeeecd52b8c0a69d8
AkbarKamran007 <akbar.neeo@gmail.com>
2022-07-20 16:10:51 +0500

    testing committ

--- dist/Api/V1/routes/test.js
+++ dist/Api/V1/routes/test.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
-router.post("/test", (req, res) => {
-    res.status(200).json({ status: "success", message: "Welcome Tourish" });
+router.get("/test", (req, res) => {
+    res.status(200).json({ status: "success", message: "Welcome to Tourish" });
 });
 exports.default = router;
 //# sourceMappingURL=test.js.map
--- src/Api/V1/routes/test.ts
+++ src/Api/V1/routes/test.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response, NextFunction } from "express";
 const router = Router();
 router.get("/test", (req: Request, res: Response) => {
-  res.status(200).json({ status: "success", message: "Welcome Tourish" });
+  res.status(200).json({ status: "success", message: "Welcome to Tourish" });
 });
 
 export default router;
